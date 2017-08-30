15bc1c54776eac0331cb772396aa42145819f321
Hermann <blumh@student.ethz.ch>
2017-08-30 11:55:40 +0200

    add test for trainign run

--- xview/models/test_fcn.py
+++ xview/models/test_fcn.py
@@ -1,4 +1,5 @@
 from .fcn import FCN
+from xview.datasets.synthia import Synthia
 
 
 def test_can_build_model():
@@ -6,8 +7,19 @@ def test_can_build_model():
     net.close()
     assert True
 
+
 def test_can_export_and_import_weights():
     with FCN(num_classes=1, num_units=64) as net:
         path = net.export_weights(save_dir='/tmp/')
         net.import_weights(path)
         assert True
+
+
+def test_can_run_training():
+    data = Synthia(['UNITTEST-SEQUENCE'], 2)
+    config = {'num_classes': 14,
+              'num_units': 5,
+              'dropout_rate': 0.2,
+              'learning_rate': 0.01}
+    with FCN(**config) as net:
+        net.fit(data, 1)
