d15b3661ea2d27ff5f72f384bbc0b5ff33f6bea1
Hermann <blumh@student.ethz.ch>
2017-09-01 16:17:24 +0200

    add more documentation to experiment data class

--- experiments/utils.py
+++ experiments/utils.py
@@ -15,8 +15,10 @@ def get_mongo_observer():
 
 
 class ExperimentData:
+    """Loads experimental data from experiments database."""
 
     def __init__(self, exp_id):
+        """Load data for experiment with id 'exp_id'."""
         client = MongoClient('mongodb://{user}:{pwd}@{host}/{db}'.format(
                              host=EXPERIMENT_DB_HOST, user=EXPERIMENT_DB_USER,
                              pwd=EXPERIMENT_DB_PWD, db=EXPERIMENT_DB_NAME))
@@ -25,9 +27,11 @@ class ExperimentData:
         self.record = self.db.runs.find_one({'_id': exp_id})
 
     def get_record(self):
+        """Get sacred record for experiment."""
         return self.record
 
     def get_artifact(self, name):
+        """Return the produced outputfile with given name as file-like object."""
         if name not in [artifact['name'] for artifact in self.record['artifacts']]:
             raise UserWarning('ERROR: Artifact {} not found'.format(name))
 
@@ -36,6 +40,7 @@ class ExperimentData:
         return self.fs.get(artifact_id)
 
     def get_summary(self, tag):
+        """Return pd.Series of scalar summary value with given tag."""
         search = [artifact['name'] for artifact in self.record['artifacts']
                   if 'events' in artifact['name']]
         if not len(search) > 0:
@@ -50,8 +55,8 @@ class ExperimentData:
         step = []
         value = []
         for event in iterator:
-              for measurement in event.summary.value:
-                    if (measurement.tag == tag):
-                        step.append(event.step)
-                        value.append(measurement.simple_value)
+            for measurement in event.summary.value:
+                if (measurement.tag == tag):
+                    step.append(event.step)
+                    value.append(measurement.simple_value)
         return Series(value, index=step)
