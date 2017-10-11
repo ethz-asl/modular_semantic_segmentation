c6865511e0245aeb46ee97695cf44343aa9ddb99
Hermann <blumh@student.ethz.ch>
2017-10-11 10:57:13 +0200

    add progressive_fcn to model-finder

--- xview/models/__init__.py
+++ xview/models/__init__.py
@@ -2,6 +2,7 @@ from .simple_fcn import SimpleFCN
 from .fcn import FCN
 from .split_fcn import SplitFCN
 from .mix_fcn import MixFCN
+from .progressive_fcn import ProgressiveFCN
 
 
 def get_model(name):
@@ -13,5 +14,7 @@ def get_model(name):
         return SplitFCN
     elif name == 'mix_fcn':
         return MixFCN
+    elif name == 'progressive_fcn':
+        return ProgressiveFCN
     else:
         raise UserWarning('ERROR: Model {} not found'.format(name))
