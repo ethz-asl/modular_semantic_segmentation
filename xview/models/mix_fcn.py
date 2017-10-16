baeeb86ba15ae5d53f00f52e6b99724ef08836d2
Hermann <blumh@student.ethz.ch>
2017-10-16 17:58:01 +0200

    first try

--- xview/models/mix_fcn.py
+++ xview/models/mix_fcn.py
@@ -3,7 +3,28 @@ import numpy as np
 from experiments.utils import ExperimentData
 
 from .base_model import BaseModel
-from xview.models import SimpleFCN
+from xview.models.simple_fcn import encoder, decoder
+
+
+def bayes_fusion(classifications, confusion_matrices):
+    # We will collect all posteriors in this list
+    log_posteriors = []
+
+    for i_expert in range(len(confusion_matrices)):
+        # compute p(expert output | groudn truth class x)
+        confusion_matrix = confusion_matrices[i_expert]
+        conditional = confusion_matrix / confusion_matrix.sum(0)
+        # likelihood is conditional at the row of the output class
+        likelihood = tf.gather(conditional, classifications[i_expert], axis=0)
+
+        # set a uniform prior for all classes
+        prior = 1.0 / 14
+
+        log_posterior = tf.log(likelihood * prior /
+                               tf.reduce_sum(likelihood * prior, axis=-1))
+        log_posteriors.append(log_posterior)
+
+    return tf.reduce_sum(tf.stack(log_posteriors, axis=0), axis=0)
 
 
 class MixFCN(BaseModel):
@@ -15,7 +36,6 @@ class MixFCN(BaseModel):
             'batch_normalization': False
         }
         standard_config.update(config)
-        self.fcn = SimpleFCN(num_channels=0, modality='none', **standard_config)
 
         # load confusion matrices
         rgb_confusion = np.load(
@@ -45,64 +65,21 @@ class MixFCN(BaseModel):
         batch_shape = tf.shape(self.test_X_rgb)
 
         def test_pipeline(inputs, prefix):
-            # Load the confusion matrix of this expert.
-            confusion = self.confusion[prefix]
-            # Norm it to rows summing up to 1 and bring it into the same shape as the
-            # batches we evaluate.
-            normed_confusion = confusion / confusion.sum(1)
-            normed_confusion = tf.tile(tf.reshape(normed_confusion, [1, 1, 1, 14, 14]),
-                                       [batch_shape[0], batch_shape[1], batch_shape[2],
-                                        1, 1])
-
             # Now we get the network output of the FCN expert.
-            features = self.fcn._encoder(inputs, prefix, reuse=False)
-            score = self.fcn._decoder(features, prefix, 0.0, reuse=False)
+            features = encoder(inputs, prefix, self.config, reuse=False)
+            score = decoder(features, prefix, 0.0, self.config, reuse=False)
             prob = tf.nn.softmax(score)
-            # Based on the confusion matrix, we find the believe about the pixel class by
-            # multiplying the expert output e(x) with the probability of p(x in C|e(x)),
-            # which we get from the normed confusion matrix above.
-            believe = tf.tile(tf.expand_dims(prob, axis=-2), [1, 1, 1, 14, 1])
-            believe = tf.reduce_sum(normed_confusion * believe, axis=-1)
-            return prob, believe
+            return prob
 
-        rgb_prob, rgb_bel = test_pipeline(self.test_X_rgb, 'rgb')
-        depth_prob, depth_bel = test_pipeline(self.test_X_d, 'depth')
+        rgb_prob = test_pipeline(self.test_X_rgb, 'rgb')
+        depth_prob = test_pipeline(self.test_X_d, 'depth')
 
-        # Based on the relative precision of the experts and their output probabilities,
-        # we find a weight per expert and pixel.
-        rgb_precision = np.diag(self.confusion['rgb']) / self.confusion['rgb'].sum(1)
-        rgb_precision = np.reshape(rgb_precision, (1, 1, 1, 14))
-        depth_precision = np.diag(self.confusion['depth']) / \
-            self.confusion['depth'].sum(1)
-        depth_precision = np.reshape(depth_precision, (1, 1, 1, 14))
-        relative_precision = np.stack([rgb_precision, depth_precision], axis=0)
-        relative_precision = relative_precision / relative_precision.sum(0)
-        # Now reshape this precision according to batch shape
-        relative_precision = np.reshape(relative_precision, (2, 1, 1, 14))
-        relative_rgb_precision = tf.tile(relative_precision[:1], [batch_shape[0],
-                                         batch_shape[1], batch_shape[2], 1])
-        relative_depth_precision = tf.tile(relative_precision[1:], [batch_shape[0],
-                                           batch_shape[1], batch_shape[2], 1])
-        rgb_weight = tf.reduce_sum(rgb_prob * relative_rgb_precision, axis=3,
-                                   keep_dims=True)
-        depth_weight = tf.reduce_sum(depth_prob * relative_depth_precision, axis=3,
-                                     keep_dims=True)
-        # Normalize prior with softmax
-        weights = tf.nn.softmax(float(self.config['separation_factor']) *
-                                tf.stack([rgb_weight, depth_weight], axis=0), dim=0)
-        rgb_weight = weights[0, :, :, :, :]
-        depth_weight = weights[1, :, :, :, :]
-
-        # Now we take the weighted sum over all expert believes as network output.
-        # The 'prior' of each expert is the previously calculated weight
-        fused_score = rgb_bel * rgb_weight + depth_bel * depth_weight
-
-        label = tf.argmax(fused_score, 3, name='label_2d')
-
-        # We also want to make predictions in the absense of one sensor modality.
-        # Therefore, we produce the standard classifications of each single expert.
         rgb_label = tf.argmax(rgb_prob, 3, name='rgb_label_2d')
         depth_label = tf.argmax(depth_prob, 3, name='depth_label_2d')
+
+        fused_score = bayes_fusion([rgb_label, depth_label],
+                                   [self.confusion[x] for x in ['rgb', 'depth']])
+        label = tf.argmax(fused_score, 3, name='label_2d')
 
         # Now we test which input data is available and based on this we produce the
         # corresponding prediction
@@ -116,12 +93,8 @@ class MixFCN(BaseModel):
         self.prediction = label
         # To understand what"s going on under the hood, we expose a lot of intermediate
         # results for evaluation
-        self.rgb_branch = {'label': rgb_label,
-                           'believe': rgb_bel,
-                           'weight': rgb_weight}
-        self.depth_branch = {'label': depth_label,
-                             'believe': depth_bel,
-                             'weight': depth_weight}
+        self.rgb_branch = {'label': rgb_label}
+        self.depth_branch = {'label': depth_label}
 
     def _enqueue_batch(self, batch, sess):
         # This model does not support training
