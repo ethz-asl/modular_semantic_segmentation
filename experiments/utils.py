5ce7cc7f11156981da409eae20515fe303670ed0
Skydes <paul.edouard.sarlin@gmail.com>
2017-10-23 21:16:36 +0200

    Fix database name

--- experiments/utils.py
+++ experiments/utils.py
@@ -11,7 +11,7 @@ def get_mongo_observer():
     return MongoObserver.create(url='mongodb://{user}:{pwd}@{host}/{db}'.format(
                                     host=EXPERIMENT_DB_HOST, user=EXPERIMENT_DB_USER,
                                     pwd=EXPERIMENT_DB_PWD, db=EXPERIMENT_DB_NAME),
-                                db_name='xview_experiments')
+                                db_name=EXPERIMENT_DB_NAME)
 
 
 class ExperimentData:
@@ -22,7 +22,7 @@ class ExperimentData:
         client = MongoClient('mongodb://{user}:{pwd}@{host}/{db}'.format(
                              host=EXPERIMENT_DB_HOST, user=EXPERIMENT_DB_USER,
                              pwd=EXPERIMENT_DB_PWD, db=EXPERIMENT_DB_NAME))
-        self.db = client['xview_experiments']
+        self.db = client[EXPERIMENT_DB_NAME]
         self.fs = GridFS(self.db)
         self.record = self.db.runs.find_one({'_id': exp_id})
 
