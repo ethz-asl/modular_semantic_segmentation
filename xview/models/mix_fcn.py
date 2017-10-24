8d509a2e303a3f2b414f6ed93a1b65b29c19f4bb
Hermann <blumh@student.ethz.ch>
2017-10-24 15:06:21 +0200

    hopefully improve performance for measurement with input queue

--- xview/models/mix_fcn.py
+++ xview/models/mix_fcn.py
@@ -3,6 +3,7 @@ import numpy as np
 from experiments.utils import ExperimentData
 from os import path
 from types import GeneratorType
+import threading
 
 from .dirichletEstimation import findDirichletPriors
 from .base_model import BaseModel
@@ -116,15 +117,65 @@ class MixFCN(BaseModel):
                                       lambda: depth_label)
             self.prediction = label
         else:
+            num_classes = self.config['num_classes']
+
+            # Build a training pipeline for measuring the differnet classifiers
+            def train_pipeline(inputs, prefix, labels):
+                # Now we get the network output of the FCN expert.
+                outputs = {}
+                outputs.update(encoder(inputs, prefix, self.config, reuse=True))
+                outputs.update(decoder(outputs['fused'], prefix, 0.0, self.config,
+                                       reuse=True))
+                prob = tf.nn.softmax(outputs['score'])
+
+                stacked_labels = tf.stack([labels for _ in range(num_classes)], axis=-1)
+
+                eps = 1e-10
+                sufficient_statistics = [tf.log(eps + tf.where(stacked_labels == c, prob,
+                                                              tf.ones_like(prob)))
+                                        for c in range(num_classes)]
+                combined = tf.stack([tf.reduce_sum(stat, axis=[0, 1, 2])
+                                     for stat in sufficient_statistics], axis=0)
+                return combined
+
+            self.train_rgb = tf.placeholder(tf.float32, shape=[None, None, None, 3])
+            self.train_depth = tf.placeholder(tf.float32, shape=[None, None, None, 1])
+            self.train_Y = tf.placeholder(tf.float32, shape=[None, None, None])
+            # An input queue is defined to load the data for several batches in advance and
+            # keep the gpu as busy as we can.
+            # IMPORTANT: The size of this queue can grow big very easily with growing
+            # batchsize, therefore do not make the queue too long, otherwise we risk getting
+            # killed by the OS
+            q = tf.FIFOQueue(3, [tf.float32, tf.float32, tf.float32])
+            self.enqueue_op = q.enqueue([self.train_rgb, self.train_depth, self.train_Y])
+            train_rgb, train_depth, training_labels = q.dequeue()
+            # The queue output does not have a defined shape, so we have to define it here to
+            # be compatible with tf.layers.
+            train_rgb.set_shape([None, None, None, 3])
+            train_depth.set_shape([None, None, None, 1])
+
+            # This operation has to be called to close the input queue and free the space it
+            # occupies in memory.
+            self.close_queue_op = q.close(cancel_pending_enqueues=True)
+            self.queue_is_finished_op = tf.Print(tf.logical_and(q.is_closed(),
+                                                       tf.equal(q.size(), 0)), [q.size()])
+
+            self.rgb_sufficient_statistic = train_pipeline(train_rgb, 'rgb',
+                                                           training_labels)
+            self.depth_sufficient_statistic = train_pipeline(train_depth, 'depth',
+                                                             training_labels)
+            self.class_counts = tf.stack(
+                [tf.reduce_sum(tf.cast(tf.equal(training_labels, c), tf.int16))
+                 for c in range(num_classes)])
+
+            # For compliance with base_model, we have to define a prediction outcome.
+            # As we do not yet know how to do fusion, we simply take rgb.
             self.prediction = rgb_label
+
         # To understand what"s going on under the hood, we expose a lot of intermediate
         # results for evaluation
         self.rgb_branch = {'label': rgb_label}
         self.depth_branch = {'label': depth_label}
-
-    def _enqueue_batch(self, batch, sess):
-        # This model does not support training
-        pass
 
     def fit(self, data):
         """Measure the encoder outputs against the given groundtruth for the given data.
@@ -134,49 +185,32 @@ class MixFCN(BaseModel):
         """
         num_classes = self.config['num_classes']
 
-        def get_sufficient_statistic(probs, labels, eps=1e-10):
-            """To infer the hidden Dirichlet distribution, sum all prob vectors belonging
-            to a given ground-truth label togehter to sufficient statistics."""
-            sufficient_statistic = np.zeros((num_classes, num_classes))
-            for c in range(num_classes):
-                this_class = np.where(
-                    np.stack([labels for _ in range(num_classes)], axis=-1) == c, probs,
-                    np.ones_like(probs))
-                # Sum the probability putput over batch and x, y coords.
-                sufficient_statistic[:, c] += np.log(eps + this_class).sum((0, 1, 2))
-            return sufficient_statistic
-
-        def get_classcounts(labels):
-            """Count the number of occurences per class."""
-            counts = np.zeros(num_classes)
-            for c in range(num_classes):
-                counts[c] = (labels == c).sum()
-            return counts
-
         with self.graph.as_default():
             # store all measurements in these matrices
             rgb_measurements = np.zeros((num_classes, num_classes))
             depth_measurements = np.zeros((num_classes, num_classes))
             class_counts = np.zeros(num_classes)
 
-            if isinstance(data, GeneratorType):
-                for batch in data:
-                    rgb_prob, depth_prob = self.sess.run(
-                        [self.rgb_prob, self.depth_prob],
-                        feed_dict=self._evaluation_food(batch))
-                    rgb_measurements += get_sufficient_statistic(rgb_prob,
-                                                                 batch['labels'])
-                    depth_measurements += get_sufficient_statistic(depth_prob,
-                                                                   batch['labels'])
-                    class_counts += get_classcounts(batch['labels'])
-            else:
-                rgb_prob, depth_prob = self.sess.run(
-                    [self.rgb_prob, self.depth_prob],
-                    feed_dict=self._evaluation_food(data))
-                rgb_measurements += get_sufficient_statistic(rgb_prob, data['labels'])
-                depth_measurements += get_sufficient_statistic(depth_prob,
-                                                               data['labels'])
-                class_counts += get_classcounts(data['labels'])
+            # Create a thread to load data.
+            coord = tf.train.Coordinator()
+            t = threading.Thread(target=self._load_and_enqueue,
+                                 args=(self.sess, data))
+            t.start()
+
+            queue_finished = False
+            while not queue_finished:
+                new_rgb, new_depth, new_count = self.sess.run(
+                    [self.rgb_sufficient_statistic, self.depth_sufficient_statistic,
+                     self.class_counts])
+                rgb_measurements += new_rgb
+                depth_measurements += new_depth
+                class_counts += new_count
+                queue_finished = self.sess.run(self.queue_is_finished_op)
+                print(queue_finished)
+
+            coord.join([t])
+
+        print('INFO: Measurements of classifiers finished, now EM')
 
         # Now, given the sufficient statistic, run Expectation-Maximization to get the
         # Dirichlet parameters
@@ -209,6 +243,34 @@ class MixFCN(BaseModel):
     def _evaluation_food(self, data):
         feed_dict = {self.test_X_rgb: data['rgb'], self.test_X_d: data['depth']}
         return feed_dict
+
+    def _enqueue_batch(self, batch, sess):
+        with self.graph.as_default():
+            feed_dict = {self.train_rgb: batch['rgb'], self.train_depth: batch['depth'],
+                         self.train_Y: batch['labels']}
+            sess.run(self.enqueue_op, feed_dict=feed_dict)
+
+    def _load_and_enqueue(self, sess, data):
+        """Internal handler method for the input data queue. Will run in a seperate
+        thread.
+
+        Overwritten from base_model because we only want to go once through the data.
+
+        Args:
+            sess: The current session, needs to be the same as in the main thread.
+            data: The data to load. See method predict for specifications.
+        """
+        with self.graph.as_default():
+            # We enqueue new data until it tells us to stop.
+            try:
+                if isinstance(data, GeneratorType):
+                    for batch in data:
+                        self._enqueue_batch(batch, sess)
+                else:
+                    self._enqueue_batch(data, sess)
+            except tf.errors.CancelledError:
+                print('INFO: Input queue is closed, cannot enqueue any more data.')
+            sess.run(self.close_queue_op)
 
     def prediction_difference(self, data):
         """Evaluate prediction of the different individual branches for the given data.
