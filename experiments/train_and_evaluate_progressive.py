3c1edc604248751115687ddf2eb1c2a8ad292ab6
Hermann <blumh@student.ethz.ch>
2017-11-13 16:18:23 +0100

    add experiment for transfer from RGB to Depth

--- experiments/train_and_evaluate_progressive.py
+++ experiments/train_and_evaluate_progressive.py
@@ -1,7 +1,8 @@
 from sacred import Experiment
 from xview.models.progressive_fcn import ProgressiveFCN
+import numpy as np
 
-from experiments.utils import get_mongo_observer
+from experiments.utils import get_mongo_observer, ExperimentData
 from experiments.fcn_train import create_directories, train_network
 from experiments.evaluation import evaluate
 
@@ -16,8 +17,45 @@ def captured_train_network(net, output_dir, data_config, num_iterations,
     train_network(net, output_dir, data_config, num_iterations, starting_weights, ex)
 
 
+@ex.command
+def rgb_to_depth(net_config, data_config, evaluation_data, starting_weights,
+                 num_iterations, _run):
+    """Training for progressive FCN with transfer from existing RGB column to Depth."""
+    # Set up the directories for diagnostics
+    output_dir = create_directories(_run._id, ex)
+
+    # Get the existing RGB weights
+    training_experiment = ExperimentData(starting_weights['experiment_id'])
+    # Take the first weights file there is in this experiment
+    filename = (artifact['name']
+                for artifact in training_experiment.get_record()['artifacts']
+                if 'weights' in artifact['name']).next()
+    rgb_weights = np.load(training_experiment.get_artifact(filename))
+
+    # For the first layer, take the mean over all 3 channels
+    rgb_weights['rgb_conv1_1/kernel'] = rgb_weights['rgb_conv1_1/kernel'].mean(3)
+
+    # We need a file handler for this new weights dict, therefore we save the weights
+    # into a temporary file.
+    np.savez('/tmp/translated_rgb_weights.npz', **rgb_weights)
+
+    # create the network
+    with ProgressiveFCN(output_dir=output_dir, **net_config) as net:
+        # import the above created weights
+        net.import_weights('/tmp/translated_rgb_weights.npz')
+
+        train_network(net, output_dir, data_config, num_iterations,
+                      starting_weights=False, experiment=ex)
+
+        print('INFO Evaluate the network adainst the training sequences')
+        evaluate(net, data_config)
+
+        print('INFO: Evaluating against seperate data')
+        evaluate(net, evaluation_data)
+
+
 @ex.automain
-def my_main(fcn_config, data_config, evaluation_data, _run):
+def main(fcn_config, data_config, evaluation_data, _run):
     """Training for progressive FCN."""
     # Set up the directories for diagnostics
     output_dir = create_directories(_run._id, ex)
