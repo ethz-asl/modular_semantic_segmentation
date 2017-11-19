6699dae2a8b93a5c4a978b9a6192fd07bc915ea3
hermann <blumh@student.ethz.ch>
2017-11-19 17:37:09 +0100

    validate finetuning network on all synthia sequences independently

--- experiments/finetuning.py
+++ experiments/finetuning.py
@@ -1,10 +1,12 @@
 from sacred import Experiment
 from xview.models.simple_fcn import SimpleFCN
 import numpy as np
+from copy import deepcopy
+from xview.datasets.synthia import AVAILABLE_SEQUENCES
 
 from experiments.utils import ExperimentData, get_mongo_observer
-from experiments.fcn_training import create_directories, train_network
-from experiments.evaluation import evaluate
+from experiments.training import create_directories, train_network, load_data
+from experiments.evaluation import evaluate, evaluate_on_all_synthia_seqs
 
 ex = Experiment()
 ex.observers.append(get_mongo_observer())
@@ -16,6 +18,15 @@ def rgb_to_depth(net_config, data_config, num_iterations, starting_weights,
     """Training for progressive FCN."""
     # Set up the directories for diagnostics
     output_dir = create_directories(_run._id, ex)
+
+    # Get data from all synthia sequences
+    all_sequences = {}
+    if data_config['dataset'] == 'synthia':
+        adapted_config = deepcopy(data_config)
+        for sequence in AVAILABLE_SEQUENCES:
+            adapted_config['seqs'] = [sequence]
+            data = load_data(adapted_config)
+            all_sequences[sequence] = data.get_validation_data(num_items=8)
 
     # Get the existing RGB weights
     training_experiment = ExperimentData(starting_weights['experiment_id'])
@@ -40,10 +51,11 @@ def rgb_to_depth(net_config, data_config, num_iterations, starting_weights,
         net.import_weights('/tmp/translated_rgb_weights.npz')
 
         train_network(net, output_dir, data_config, num_iterations,
-                      starting_weights=False, experiment=ex)
+                      starting_weights=False, experiment=ex,
+                      additional_eval_data=all_sequences)
 
         print('INFO: Evaluate the network adainst the training sequences')
         evaluate(net, data_config)
 
         print('INFO: Evaluating against seperate data')
-        evaluate(net, evaluation_data)
+        evaluate_on_all_synthia_seqs(net, data_config)
