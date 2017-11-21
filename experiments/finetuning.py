f653bf770cb5a5cad70269f4d9ad3e5376de9326
Hermann <blumh@student.ethz.ch>
2017-11-21 15:43:54 +0100

    add depth to rgb transfer experiment

--- experiments/finetuning.py
+++ experiments/finetuning.py
@@ -12,13 +12,7 @@ ex = Experiment()
 ex.observers.append(get_mongo_observer())
 
 
-@ex.automain
-def rgb_to_depth(net_config, data_config, num_iterations, starting_weights, 
-                 _run):
-    """Training for progressive FCN."""
-    # Set up the directories for diagnostics
-    output_dir = create_directories(_run._id, ex)
-
+def get_all_sequence_validation_sets(data_config):
     # Get data from all synthia sequences
     all_sequences = {}
     if data_config['dataset'] == 'synthia':
@@ -26,7 +20,57 @@ def rgb_to_depth(net_config, data_config, num_iterations, starting_weights,
         for sequence in AVAILABLE_SEQUENCES:
             adapted_config['seqs'] = [sequence]
             data = load_data(adapted_config)
-            all_sequences[sequence] = data.get_validation_data(num_items=7)
+            all_sequences[sequence] = data.get_validation_data(num_items=15)
+    return all_sequences
+
+
+@ex.command
+def depth_to_rgb(net_config, data_config, num_iterations, starting_weights,
+                 _run):
+    """Training for progressive FCN."""
+    # Set up the directories for diagnostics
+    output_dir = create_directories(_run._id, ex)
+
+    # Get the existing RGB weights
+    training_experiment = ExperimentData(starting_weights['experiment_id'])
+    # Take the first weights file there is in this experiment
+    filename = (artifact['name']
+                for artifact in training_experiment.get_record()['artifacts']
+                if 'weights' in artifact['name']).next()
+    depth_weights = np.load(training_experiment.get_artifact(filename))
+
+    # For the first layer, take the mean over all 3 channels
+    # Therefore, we have to define a new weights dict.
+    new_weights = {key: depth_weights[key] for key in depth_weights}
+    new_weights['depth_conv1_1/kernel'] = np.tile(depth_weights['depth_conv1_1/kernel'],
+                                                  [1, 1, 3, 1])
+
+    # We need a file handler for this new weights dict, therefore we save the weights
+    # into a temporary file.
+    np.savez('/tmp/translated_depth_weights.npz', **new_weights)
+
+    # create the network
+    with SimpleFCN(output_dir=output_dir, **net_config) as net:
+        # import the above created weights
+        net.import_weights('/tmp/translated_depth_weights.npz')
+
+        train_network(net, output_dir, data_config, num_iterations,
+                      starting_weights=False, experiment=ex,
+                      additional_eval_data=get_all_sequence_validation_sets(data_config))
+
+        print('INFO: Evaluate the network adainst the training sequences')
+        evaluate(net, data_config)
+
+        print('INFO: Evaluating against seperate data')
+        evaluate_on_all_synthia_seqs(net, data_config)
+
+
+@ex.automain
+def rgb_to_depth(net_config, data_config, num_iterations, starting_weights,
+                 _run):
+    """Training for progressive FCN."""
+    # Set up the directories for diagnostics
+    output_dir = create_directories(_run._id, ex)
 
     # Get the existing RGB weights
     training_experiment = ExperimentData(starting_weights['experiment_id'])
@@ -39,7 +83,8 @@ def rgb_to_depth(net_config, data_config, num_iterations, starting_weights,
     # For the first layer, take the mean over all 3 channels
     # Therefore, we have to define a new weights dict.
     new_weights = {key: rgb_weights[key] for key in rgb_weights}
-    new_weights['rgb_conv1_1/kernel'] = rgb_weights['rgb_conv1_1/kernel'].mean(2, keepdims=True)
+    new_weights['rgb_conv1_1/kernel'] = \
+        rgb_weights['rgb_conv1_1/kernel'].mean(2, keepdims=True)
 
     # We need a file handler for this new weights dict, therefore we save the weights
     # into a temporary file.
@@ -52,7 +97,7 @@ def rgb_to_depth(net_config, data_config, num_iterations, starting_weights,
 
         train_network(net, output_dir, data_config, num_iterations,
                       starting_weights=False, experiment=ex,
-                      additional_eval_data=all_sequences)
+                      additional_eval_data=get_all_sequence_validation_sets(data_config))
 
         print('INFO: Evaluate the network adainst the training sequences')
         evaluate(net, data_config)
