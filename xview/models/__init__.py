92a2d812b09a52103369835c27aa2b339897cad3
Hermann <blumh@student.ethz.ch>
2017-11-21 14:45:42 +0100

    remove unused models from init

--- xview/models/__init__.py
+++ xview/models/__init__.py
@@ -1,18 +1,12 @@
 from .simple_fcn import SimpleFCN
-from .fcn import FCN
-from .split_fcn import SplitFCN
 from .mix_fcn import MixFCN
 from .progressive_fcn import ProgressiveFCN
 from .adapnet import Adapnet
 
 
 def get_model(name):
-    if name == 'fcn':
-        return FCN
-    elif name == 'simple_fcn':
+    if name == 'simple_fcn':
         return SimpleFCN
-    elif name == 'split_fcn':
-        return SplitFCN
     elif name == 'mix_fcn':
         return MixFCN
     elif name == 'progressive_fcn':
