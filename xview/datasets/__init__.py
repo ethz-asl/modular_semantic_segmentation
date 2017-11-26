2668b84d5e52afb61c2b29ce9bb4ac88a13a369f
Hermann Blum <hermannsblum@yahoo.de>
2017-11-26 18:04:39 +0100

    find freiburg-forest dataset by name

--- xview/datasets/__init__.py
+++ xview/datasets/__init__.py
@@ -1,8 +1,11 @@
 from .synthia import Synthia
+from .freiburg_forest import FreiburgForest
 
 
 def get_dataset(name, config):
     if name == 'synthia':
         return Synthia(**config)
+    elif name == 'freiburg_forest':
+        return FreiburgForest(**config)
     else:
         raise UserWarning('ERROR: Dataset {} not found'.format(name))
