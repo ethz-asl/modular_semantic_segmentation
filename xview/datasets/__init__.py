d2b6b6121e5f35f505ee20d8e2f10a0e2b2f5fd2
hermann <blumh@student.ethz.ch>
2017-11-26 18:05:30 +0100

    find raw_synthia by name

--- xview/datasets/__init__.py
+++ xview/datasets/__init__.py
@@ -1,8 +1,10 @@
 from .synthia import Synthia
-
+from .raw_synthia import Synthia as SynthiaRaw
 
 def get_dataset(name, config):
     if name == 'synthia':
         return Synthia(**config)
+    elif name == 'raw_synthia':
+        return SynthiaRaw(**config)
     else:
         raise UserWarning('ERROR: Dataset {} not found'.format(name))
