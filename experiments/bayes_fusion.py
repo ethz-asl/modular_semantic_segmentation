a617c9f791308ac35a621fa4675cdb8aa868a82f
Hermann <blumh@student.ethz.ch>
2018-02-20 13:49:58 +0100

    add evaluation script to evaluate experts and then fuse them

---
+++ experiments/bayes_fusion.py
@@ -0,0 +1,51 @@
+from sacred import Experiment
+from experiments.utils import get_mongo_observer
+from experiments.evaluation import evaluate, import_weights_into_network
+from xview.datasets import get_dataset
+from xview.models import get_model, BayesMix
+from copy import deepcopy
+
+
+ex = Experiment()
+ex.observers.append(get_mongo_observer())
+
+@ex.automain
+def fit_and_evaluate(net_config, evaluation_data, starting_weights, _run):
+    """Load weigths from training experiments and evalaute fusion against specified
+    data."""
+
+    # Load the dataset, we expect config to include the arguments
+    dataset_params = {key: val for key, val in evaluation_data.items()
+                      if key not in ['dataset', 'use_trainset']}
+    data = get_dataset(evaluation_data['dataset'], dataset_params)
+
+    # evaluate individual experts
+    model = get_model(net_config['expert_model'])
+    confusion_matrices = {}
+    for expert in net_config['num_channels']:
+        model_config = deepcopy(net_config)
+        model_config['num_channels'] = net_config['num_channels'][expert]
+        model_config['modaliy'] = expert
+        model_config['prefix'] = net_config['prefixes'][expert]
+        with model(**model_config) as net:
+            import_weights_into_network(net, starting_weights[model_config['prefix']])
+            if evaluation_data['use_trainset']:
+                m, conf_mat = net.score(data.get_train_data())
+            else:
+                m, conf_mat = net.score(data.get_validation_data()())
+            confusion_matrices[expert] = conf_mat
+            print('Evaluated network {} on {} train data:'.format(
+                expert, evaluation_data['dataset']))
+            print('total accuracy {:.3f} IoU {:.3f}'.format(m['total_accuracy'],
+                                                            m['mean_IoU']))
+    _run.info['confusion_matrices'] = confusion_matrices
+
+    # now evaluate bayes mix
+    with BayesMix(confusion_matrices=confusion_matrices, **net_config) as net:
+        import_weights_into_network(net, starting_weights)
+
+        # never evaluate against test data
+        evaluation_data['use_trainset'] = False
+        measurements, confusion_matrix = evaluate(net, evaluation_data)
+        _run.info['measurements'] = measurements
+        _run.info['confusion_matrix'] = confusion_matrix
