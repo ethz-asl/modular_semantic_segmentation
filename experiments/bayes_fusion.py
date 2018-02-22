40c74cf52f422090d5793b97b5d8f14715192b04
Hermann <blumh@student.ethz.ch>
2018-02-22 16:22:04 +0100

    measure baselines on part of testset

--- experiments/bayes_fusion.py
+++ experiments/bayes_fusion.py
@@ -1,17 +1,19 @@
 from sacred import Experiment
 from sacred.utils import apply_backspaces_and_linefeeds
 from experiments.utils import get_mongo_observer
-from experiments.evaluation import evaluate, import_weights_into_network
+from experiments.evaluation import import_weights_into_network
 from xview.datasets import get_dataset
 from xview.models import get_model, BayesMix
 from copy import deepcopy
 from sys import stdout
+from sklearn.model_selection import train_test_split
 
 
 ex = Experiment()
 # reduce output of progress bars
 ex.captured_out_filter = apply_backspaces_and_linefeeds
 ex.observers.append(get_mongo_observer())
+
 
 @ex.automain
 def fit_and_evaluate(net_config, evaluation_data, starting_weights, _run):
@@ -20,11 +22,13 @@ def fit_and_evaluate(net_config, evaluation_data, starting_weights, _run):
 
     # Load the dataset, we expect config to include the arguments
     dataset_params = {key: val for key, val in evaluation_data.items()
-                      if key not in ['dataset', 'use_trainset']}
+                      if key not in ['dataset']}
     dataset_params['augmentation'] = {
         key: False for key in ['crop', 'scale', 'vflip', 'hflip', 'gamma', 'rotate',
                                'shear', 'contrast', 'brightness']}
     data = get_dataset(evaluation_data['dataset'], dataset_params)
+
+    measure_set, test_set = train_test_split(data.testset, random_state=1)
 
     # evaluate individual experts
     model = get_model(net_config['expert_model'])
@@ -36,10 +40,7 @@ def fit_and_evaluate(net_config, evaluation_data, starting_weights, _run):
         model_config['prefix'] = net_config['prefixes'][expert]
         with model(**model_config) as net:
             import_weights_into_network(net, starting_weights[model_config['prefix']])
-            if evaluation_data['use_trainset']:
-                m, conf_mat = net.score(data.get_train_data(training_format=False))
-            else:
-                m, conf_mat = net.score(data.get_validation_data()())
+            m, conf_mat = net.score(data.get_set_data(measure_set))
             confusion_matrices[expert] = conf_mat
             print('Evaluated network {} on {} train data:'.format(
                 expert, evaluation_data['dataset']))
@@ -50,10 +51,7 @@ def fit_and_evaluate(net_config, evaluation_data, starting_weights, _run):
     # now evaluate bayes mix
     with BayesMix(confusion_matrices=confusion_matrices, **net_config) as net:
         import_weights_into_network(net, starting_weights)
-
-        # never evaluate against train data
-        evaluation_data['use_trainset'] = False
-        measurements, confusion_matrix = evaluate(net, evaluation_data)
+        measurements, confusion_matrix = net.score(data.get_set_data(test_set))
         _run.info['measurements'] = measurements
         _run.info['confusion_matrix'] = confusion_matrix
 
