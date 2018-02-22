c95e38821a36092edc3a609bd50683c386b1d018
Hermann <blumh@student.ethz.ch>
2018-02-22 16:32:33 +0100

    split testset exactly at half

--- experiments/bayes_fusion.py
+++ experiments/bayes_fusion.py
@@ -3,7 +3,7 @@ from sacred.utils import apply_backspaces_and_linefeeds
 from experiments.utils import get_mongo_observer
 from experiments.evaluation import import_weights_into_network
 from xview.datasets import get_dataset
-from xview.models import get_model, BayesMix
+from xview.models import get_model, BayesMix, AverageMix
 from copy import deepcopy
 from sys import stdout
 from sklearn.model_selection import train_test_split
@@ -13,6 +13,36 @@ ex = Experiment()
 # reduce output of progress bars
 ex.captured_out_filter = apply_backspaces_and_linefeeds
 ex.observers.append(get_mongo_observer())
+
+
+@ex.command
+def evaluate_average(net_config, evaluation_data, starting_weights, _run):
+    """Load weigths from training experiments and evalaute fusion against specified
+    data."""
+
+    # Load the dataset, we expect config to include the arguments
+    dataset_params = {key: val for key, val in evaluation_data.items()
+                      if key not in ['dataset']}
+    dataset_params['augmentation'] = {
+        key: False for key in ['crop', 'scale', 'vflip', 'hflip', 'gamma', 'rotate',
+                               'shear', 'contrast', 'brightness']}
+    data = get_dataset(evaluation_data['dataset'], dataset_params)
+
+    _, test_set = train_test_split(data.testset, test_size=.5, random_state=1)
+
+    # now evaluate average mix
+    with BayesMix(confusion_matrices=confusion_matrices, **net_config) as net:
+        import_weights_into_network(net, starting_weights)
+        measurements, confusion_matrix = net.score(data.get_set_data(test_set))
+        _run.info['measurements'] = measurements
+        _run.info['confusion_matrix'] = confusion_matrix
+
+    print('Evaluated Bayes Fusion on {} data:'.format(evaluation_data['dataset']))
+    print('total accuracy {:.3f} IoU {:.3f}'.format(measurements['total_accuracy'],
+                                                    measurements['mean_IoU']))
+
+    # There seems to be a problem with capturing the print output, flush to be sure
+    stdout.flush()
 
 
 @ex.automain
@@ -28,7 +58,7 @@ def fit_and_evaluate(net_config, evaluation_data, starting_weights, _run):
                                'shear', 'contrast', 'brightness']}
     data = get_dataset(evaluation_data['dataset'], dataset_params)
 
-    measure_set, test_set = train_test_split(data.testset, random_state=1)
+    measure_set, test_set = train_test_split(data.testset, test_size=.5, random_state=1)
 
     # evaluate individual experts
     model = get_model(net_config['expert_model'])
