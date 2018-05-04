0a7455051945a27ef2ab016d3b95cbf49ecbe88c
Hermann <blumh@ethz.ch>
2018-05-04 15:58:39 +0200

    unpooling layer, even if it is not needed right now

--- xview/models/custom_layers.py
+++ xview/models/custom_layers.py
@@ -242,3 +242,48 @@ def entropy(x, axis=-1):
                 / tf.log(tf.to_float(tf.shape(x)[axis]))
     return entropy
 
+
+def unpool_2d(pool, ind, stride=[1, 2, 2, 1], scope='unpool_2d'):
+    """Adds a 2D unpooling op.
+    https://arxiv.org/abs/1505.04366
+    Implementation found here: https://github.com/rayanelleuch/tensorflow/blob/
+    b46d50583d8f4893f1b1d629d0ac9cb2cff580af/tensorflow/contrib/layers/python/layers/
+    layers.py#L2291-L2327
+    Tensorflow does not yet have an own implementation, issue is discussed here:
+    https://github.com/tensorflow/tensorflow/issues/2169
+
+    Unpooling layer after max_pool_with_argmax.
+        Args:
+           pool:        max pooled output tensor
+           ind:         argmax indices
+           stride:      stride is the same as for the pool
+        Return:
+           unpool:    unpooling tensor
+    """
+    with tf.variable_scope(scope):
+        input_shape = tf.shape(pool)
+        output_shape = [input_shape[0], input_shape[1] * stride[1],
+                        input_shape[2] * stride[2], input_shape[3]]
+
+        flat_input_size = tf.reduce_prod(input_shape)
+        flat_output_shape = [output_shape[0],
+                             output_shape[1] * output_shape[2] * output_shape[3]]
+
+        pool_ = tf.reshape(pool, [flat_input_size])
+        batch_range = tf.reshape(tf.range(tf.cast(output_shape[0], tf.int64),
+                                          dtype=ind.dtype),
+                                 shape=[input_shape[0], 1, 1, 1])
+        b = tf.ones_like(ind) * batch_range
+        b1 = tf.reshape(b, [flat_input_size, 1])
+        ind_ = tf.reshape(ind, [flat_input_size, 1])
+        ind_ = tf.concat([b1, ind_], 1)
+
+        ret = tf.scatter_nd(ind_, pool_, shape=tf.cast(flat_output_shape, tf.int64))
+        ret = tf.reshape(ret, output_shape)
+
+        set_input_shape = pool.get_shape()
+        set_output_shape = [set_input_shape[0], set_input_shape[1] * stride[1],
+                            set_input_shape[2] * stride[2], set_input_shape[3]]
+        ret.set_shape(set_output_shape)
+    return ret
+
