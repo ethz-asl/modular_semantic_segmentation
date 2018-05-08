f1f05438a05054eefa2d51a04683f6538b02fd95
Hermann <blumh@ethz.ch>
2018-05-08 13:21:50 +0200

    fix errors in bayesian fcn at the same time, refactor simple_fcn to proper variable scopes

--- xview/models/bayesian_fcn.py
+++ xview/models/bayesian_fcn.py
@@ -3,10 +3,10 @@ import tensorflow as tf
 from .base_model import BaseModel
 from .utils import cross_entropy
 from .simple_fcn import encoder, decoder
-from .custom_layers import entropy
+from .custom_layers import entropy, log_softmax
 
 
-def epistemic_uncertainty(inputs, pipeline, num_samples, first_run=False, **kwargs):
+def sampling_uncertainty(inputs, pipeline, num_samples, num_classes, **kwargs):
     """
     Samples MC-dropout samples out of the network and produces uncertainty and output
 
@@ -15,13 +15,21 @@ def epistemic_uncertainty(inputs, pipeline, num_samples, first_run=False, **kwar
     Returns:
         Tuple, first item is class probability, second is uncertainty
     """
-    counter = tf.constant(1, dtype=tf.int32, name='epistemic_sample_counter')
+    """
+    counter = tf.constant(0, name='sample_counter')
 
     # we produce a first sample to get to know the output shape and initialize the
     # tensor of samples
-    samples = tf.expand_dims(pipeline(inputs, reuse=(not first_run), **kwargs), axis=0)
+    #samples = tf.expand_dims(pipeline(inputs, **kwargs)['prob'], axis=0)
+    
+    out_shape = tf.concat([tf.shape(inputs)[:-1],
+                           tf.convert_to_tensor([num_classes])], axis=0)
+    samples = tf.zeros(tf.concat([tf.convert_to_tensor([0]), out_shape], axis=0))
+
     # set the general shape of the tensors
-    samples_shape = tf.concat([None, tf.shape(samples)[1:]])
+    samples_shape = samples.shape
+    samples_shape = tf.TensorShape(
+        [None, samples_shape[1], samples_shape[2], samples_shape[3], num_classes])
 
     def loop_cond(counter, samples):
         # the loop runs until <num_samples> samples have been produced
@@ -29,15 +37,24 @@ def epistemic_uncertainty(inputs, pipeline, num_samples, first_run=False, **kwar
 
     def loop_body(counter, samples):
         # each loop iteration adds 1 sample
-        sample = pipeline(inputs, **kwargs)
+        sample = pipeline(inputs, **kwargs)['prob']
         return tf.add(counter, 1), tf.concat([samples, tf.expand_dims(sample, axis=0)],
                                              axis=0)
 
     # now let the loop run to produce all the samples
     _, samples = tf.while_loop(loop_cond, loop_body, [counter, samples],
                                shape_invariants=[counter.get_shape(), samples_shape])
+    """
+    with tf.name_scope('samples'):
+        samples = tf.stack([pipeline(inputs, **kwargs)['prob']
+                            for _ in range(num_samples)], axis=0)
     mean = tf.reduce_mean(samples, axis=0)
-    return mean, entropy(mean)
+    uncertainties = {
+        'entropy': entropy(mean),
+        'cond_entropy': tf.reduce_mean(entropy(samples), axis=0),
+        'variance': tf.reduce_sum(tf.nn.moments(samples, axes=[0])[1], axis=-1)
+    }
+    return mean, uncertainties
 
 
 class BayesianFCN(BaseModel):
@@ -65,41 +82,38 @@ class BayesianFCN(BaseModel):
     def _build_graph(self, train_data, test_data):
         """Builds the whole network. Network is split into 2 similar pipelines with shared
         weights, one for training and one for testing."""
-
-        def sample_pipeline(inputs, is_training=False, reuse=True):
-            # this is 1 monte-carlo sample
-            encoder_layers = encoder(
+        def sample_pipeline(inputs, is_training=False, reuse=tf.AUTO_REUSE):
+            layers = encoder(
                 inputs, self.prefix, self.config['num_units'],
-                self.config['dropout_rate'], reuse=reuse,
+                self.config['dropout_rate'], is_training=is_training, reuse=reuse,
                 dropout_layers=self.config['dropout_layers'])
-            decoder_layers = decoder(
-                encoder_layers['fused'], self.prefix, self.config['num_units'],
-                self.config['num_classes'], is_training=True, reuse=reuse,
+            layers.update(decoder(
+                layers['fused'], self.prefix, self.config['num_units'],
+                self.config['num_classes'], is_training=is_training, reuse=reuse,
                 dropout_rate=(self.config['dropout_rate']
                               if 'features' in self.config['dropout_layers']
-                              else None))
-            prob = tf.nn.softmax(decoder_layers['score'])
-            return prob
+                              else None)))
+            layers['prob'] = tf.nn.softmax(layers['score'])
+            return layers
 
-        # Training input
-        train_x = train_data[self.modality]
-        # ground truth labels
-        train_y = tf.to_float(train_data['labels'])
-
-        # Network for testing / evaluation
-        test_x = test_data[self.modality]
-
-        if self.config['method'] == 'epistemic':
+        if self.config['method'] == 'sampling':
             # training pipeline
-            mean, _ = epistemic_uncertainty(
-                train_x, sample_pipeline, self.config['num_samples'], is_training=True,
-                first_run=True)
-            self.loss = tf.div(tf.reduce_sum(cross_entropy(tf.log(mean), train_y)),
-                               tf.reduce_sum(train_y))
+            with tf.name_scope('training'):
+                train_x = train_data[self.modality]
+                # ground truth labels
+                train_y = tf.to_float(train_data['labels'])
+
+                score = sample_pipeline(train_x, is_training=True)['score']
+                log_prob = log_softmax(score, self.config['num_classes'])
+                self.loss = tf.div(tf.reduce_sum(cross_entropy(log_prob, train_y)),
+                                   tf.reduce_sum(train_y))
 
             # testing
-            mean, uncertainty = epistemic_uncertainty(test_x, sample_pipeline,
-                                                      self.config['num_samples'])
-            self.prob = mean
-            self.prediction = tf.argmax(mean, 3)
-            self.uncertainty = uncertainty
+            with tf.name_scope('testing'):
+                test_x = test_data[self.modality]
+                mean, uncertainties = sampling_uncertainty(
+                    test_x, sample_pipeline, self.config['num_samples'],
+                    self.config['num_classes'])
+                self.prediction = tf.argmax(mean, 3)
+                for key, uncertainty in uncertainties.items():
+                    setattr(self, key, uncertainty)
--- xview/models/simple_fcn.py
+++ xview/models/simple_fcn.py
@@ -9,7 +9,7 @@ from .vgg16 import vgg16
 
 
 def encoder(inputs, prefix, num_units, dropout_rate, trainable=True,
-            is_training=False, reuse=True, dropout_layers=[]):
+            is_training=False, reuse=tf.AUTO_REUSE, dropout_layers=[]):
     """
     VGG16 image encoder with fusion of conv4_3 and conv5_3 features.
 
@@ -34,64 +34,64 @@ def encoder(inputs, prefix, num_units, dropout_rate, trainable=True,
               'batch_normalization': True, 'training': is_training,
               'trainable': trainable}
 
-    l = vgg16(inputs, prefix, params)
+    with tf.variable_scope(prefix, reuse=reuse):
+        # dict for all the layers
+        l = {}
+        l['conv1_1'] = conv2d(inputs, 64, [3, 3], name='conv1_1', **params)
+        l['conv1_2'] = conv2d(l['conv1_1'], 64, [3, 3], name='conv1_2', **params)
+        l['pool1'] = max_pooling2d(l['conv1_2'], [2, 2], [2, 2], name='pool1')
+        l['conv2_1'] = conv2d(l['pool1'], 128, [3, 3], name='conv2_1', **params)
+        l['conv2_2'] = conv2d(l['conv2_1'], 128, [3, 3], name='conv2_2', **params)
+        l['pool2'] = max_pooling2d(l['conv2_2'], [2, 2], [2, 2], name='pool2')
+        l['conv3_1'] = conv2d(l['pool2'], 256, [3, 3], name='conv3_1', **params)
+        l['conv3_2'] = conv2d(l['conv3_1'], 256, [3, 3], name='conv3_2', **params)
+        l['conv3_3'] = conv2d(l['conv3_2'], 256, [3, 3], name='conv3_3', **params)
+        l['pool3'] = max_pooling2d(l['conv3_3'], [2, 2], [2, 2], name='pool3')
+        # dropout after pool3
+        last_layer = l['pool3']
+        if 'pool3' in dropout_layers:
+            l['pool3_drop'] = dropout(l['pool3'], rate=dropout_rate, training=True,
+                                      name='pool3_dropout')
+            last_layer = l['pool3_drop']
+        l['conv4_1'] = conv2d(last_layer, 512, [3, 3], name='conv4_1', **params)
+        l['conv4_2'] = conv2d(l['conv4_1'], 512, [3, 3], name='conv4_2', **params)
+        l['conv4_3'] = conv2d(l['conv4_2'], 512, [3, 3], name='conv4_3', **params)
+        l['pool4'] = max_pooling2d(l['conv4_3'], [2, 2], [2, 2], name='pool4')
+        # dropout after pool4
+        last_layer = l['pool4']
+        if 'pool3' in dropout_layers:
+            l['pool4_drop'] = dropout(l['pool4'], rate=dropout_rate, training=True,
+                                      name='pool4_dropout')
+            last_layer = l['pool4_drop']
+        l['conv5_1'] = conv2d(last_layer, 512, [3, 3], name='conv5_1', **params)
+        l['conv5_2'] = conv2d(l['conv5_1'], 512, [3, 3], name='conv5_2', **params)
+        l['conv5_3'] = conv2d(l['conv5_2'], 512, [3, 3], name='conv5_3', **params)
+        # Use 1x1 convolutions on conv4_3 and conv5_3 to define features.
+        # first, maybe apply dropout at these layers?
+        conv4_3 = l['conv4_3']
+        if 'conv4_3' in dropout_layers:
+            conv4_3 = dropout(conv4_3, rate=dropout_rate, training=True,
+                              name='conv4_3_dropout')
+        score_conv4 = conv2d(conv4_3, num_units, [1, 1], name='%s_score_conv4' % prefix,
+                             **params)
+        conv5_3 = l['conv5_3']
+        if 'conv5_3' in dropout_layers:
+            conv5_3 = dropout(conv5_3, rate=dropout_rate, training=True,
+                              name='conv5_3_dropout')
+        score_conv5 = conv2d(conv5_3, num_units, [1, 1], name='%s_score_conv5' % prefix,
+                             **params)
+        # The deconvolution is always set static.
+        params['trainable'] = False
+        upscore_conv5 = deconv2d(score_conv5, num_units, [4, 4], strides=[2, 2],
+                                 name='upscore_conv5', **params)
 
-    # dropout after pool3
-    last_layer = l['pool3']
-    if 'pool3' in dropout_layers:
-        l['pool3_drop'] = dropout(l['pool3'], rate=dropout_rate, training=True,
-                                  name='%s_pool3_dropout' % prefix)
-        last_layer = l['pool3_drop']
-
-    # now we have to overwrite the following layers:
-    params['reuse'] = True
-    l['conv4_1'] = conv2d(last_layer, 512, [3, 3], name='%s_conv4_1' % prefix, **params)
-    l['conv4_2'] = conv2d(l['conv4_1'], 512, [3, 3], name='%s_conv4_2' % prefix,
-                          **params)
-    l['conv4_3'] = conv2d(l['conv4_2'], 512, [3, 3], name='%s_conv4_3' % prefix,
-                          **params)
-    l['pool4'] = max_pooling2d(l['conv4_3'], [2, 2], [2, 2], name='%s_pool4' % prefix)
-    # dropout after pool4
-    last_layer = l['pool4']
-    if 'pool3' in dropout_layers:
-        l['pool4_drop'] = dropout(l['pool4'], rate=dropout_rate, training=True,
-                                  name='%S_pool4_dropout' % prefix)
-        last_layer = l['pool4_drop']
-    l['conv5_1'] = conv2d(last_layer, 512, [3, 3], name='%s_conv5_1' % prefix, **params)
-    l['conv5_2'] = conv2d(l['conv5_1'], 512, [3, 3], name='%s_conv5_2' % prefix,
-                          **params)
-    l['conv5_3'] = conv2d(l['conv5_2'], 512, [3, 3], name='%s_conv5_3' % prefix,
-                          **params)
-    params['reuse'] = reuse
-
-    # Use 1x1 convolutions on conv4_3 and conv5_3 to define features.
-    # first, maybe apply dropout at these layers?
-    conv4_3 = l['conv4_3']
-    if 'conv4_3' in dropout_layers:
-        conv4_3 = dropout(conv4_3, rate=dropout_rate, training=True,
-                          name='%s_conv4_3_dropout' % prefix)
-    score_conv4 = conv2d(conv4_3, num_units, [1, 1], name='%s_score_conv4' % prefix,
-                         **params)
-    conv5_3 = l['conv5_3']
-    if 'conv5_3' in dropout_layers:
-        conv5_3 = dropout(conv5_3, rate=dropout_rate, training=True,
-                          name='%s_conv5_3_dropout' % prefix)
-    score_conv5 = conv2d(conv5_3, num_units, [1, 1], name='%s_score_conv5' % prefix,
-                         **params)
-    # The deconvolution is always set static.
-    params['trainable'] = False
-    upscore_conv5 = deconv2d(score_conv5, num_units, [4, 4], strides=[2, 2],
-                             name='{}_upscore_conv5'.format(prefix), **params)
-
-    l['fused'] = tf.add_n([score_conv4, upscore_conv5],
-                          name='{}_add_score'.format(prefix))
-
+        l['fused'] = tf.add_n([score_conv4, upscore_conv5], name='add_score')
     # Return dictionary of all layers
     return l
 
 
 def decoder(features, prefix, num_units, num_classes, trainable=True, is_training=False,
-            reuse=True, dropout_rate=None):
+            reuse=tf.AUTO_REUSE, dropout_rate=None):
     """
     FCN feature decoder.
 
@@ -119,17 +119,17 @@ def decoder(features, prefix, num_units, num_classes, trainable=True, is_trainin
     upscore_params = deepcopy(params)
     upscore_params['trainable'] = False
 
-    if dropout_rate is not None:
-        features = dropout(features, rate=dropout_rate, training=True,
-                           name='%s_features_dropout' % prefix)
+    with tf.variable_scope(prefix, reuse=reuse):
+        if dropout_rate is not None:
+            features = dropout(features, rate=dropout_rate, training=True,
+                               name='features_dropout')
 
-    # Upsample the fused features to the output classification size
-    features = deconv2d(features, num_units, [16, 16], strides=[8, 8],
-                        name='{}_upscore'.format(prefix), **upscore_params)
-    # no activation before the softmax
-    params['activation'] = None
-    score = conv2d(features, num_classes, [1, 1],
-                   name='{}_score'.format(prefix), **params)
+        # Upsample the fused features to the output classification size
+        features = deconv2d(features, num_units, [16, 16], strides=[8, 8],
+                            name='upscore', **upscore_params)
+        # no activation before the softmax
+        params['activation'] = None
+        score = conv2d(features, num_classes, [1, 1], name='score', **params)
     return {'upscore': features, 'score': score}
 
 
@@ -162,36 +162,32 @@ class SimpleFCN(BaseModel):
         weights, one for training and one for testing."""
 
         # Training network
-        train_x = train_data[self.modality]
-        # ground truth labels
-        train_y = tf.to_float(train_data['labels'])
+        with tf.name_scope('training'):
+            train_x = train_data[self.modality]
+            # ground truth labels
+            train_y = tf.to_float(train_data['labels'])
 
-        encoder_layers = encoder(train_x, self.prefix, self.config['num_units'],
-                                 self.config['dropout_rate'], is_training=True,
-                                 reuse=False,
-                                 trainable=self.config.get('train_encoder', True))
-        decoder_layers = decoder(encoder_layers['fused'], self.prefix,
-                                 self.config['num_units'], self.config['num_classes'],
-                                 is_training=True, reuse=False)
-        prob = tf.nn.log_softmax(decoder_layers['score'], name='prob')
-        # The loss is given by the cross-entropy with the ground-truth
-        self.loss = tf.div(tf.reduce_sum(cross_entropy(prob, train_y)),
-                           tf.reduce_sum(train_y))
-        self.train_y = train_y
+            encoder_layers = encoder(train_x, self.prefix, self.config['num_units'],
+                                     self.config['dropout_rate'], is_training=True,
+                                     trainable=self.config.get('train_encoder', True))
+            decoder_layers = decoder(encoder_layers['fused'], self.prefix,
+                                     self.config['num_units'], self.config['num_classes'],
+                                     is_training=True)
+            prob = tf.nn.log_softmax(decoder_layers['score'], name='prob')
+            # The loss is given by the cross-entropy with the ground-truth
+            self.loss = tf.div(tf.reduce_sum(cross_entropy(prob, train_y)),
+                               tf.reduce_sum(train_y))
+            self.train_y = train_y
 
         # Network for testing / evaluation
-        test_x = test_data[self.modality]
-        encoder_layers = encoder(test_x, self.prefix, self.config['num_units'],
-                                 tf.constant(0.0), is_training=False, reuse=True)
-        decoder_layers = decoder(encoder_layers['fused'], self.prefix,
-                                 self.config['num_units'], self.config['num_classes'],
-                                 is_training=False, reuse=True)
-        label = tf.nn.softmax(decoder_layers['score'], name='prob_normalized')
-        self.prediction = tf.argmax(label, 3, name='label_2d')
+        with tf.name_scope('testing'):
+            test_x = test_data[self.modality]
+            encoder_layers = encoder(test_x, self.prefix, self.config['num_units'],
+                                     tf.constant(0.0), is_training=False)
+            decoder_layers = decoder(encoder_layers['fused'], self.prefix,
+                                     self.config['num_units'], self.config['num_classes'],
+                                     is_training=False)
+            label = tf.nn.softmax(decoder_layers['score'], name='prob_normalized')
+            self.prediction = tf.argmax(label, 3, name='label_2d')
 
         # Add summaries for some weights
-        variable_names = [x.format(self.prefix)
-                          for x in ['{}_score/kernel:0', '{}_score/bias:0']]
-        for name in variable_names:
-            var = next(v for v in tf.global_variables() if v.name == name)
-            tf.summary.histogram(name, var)
