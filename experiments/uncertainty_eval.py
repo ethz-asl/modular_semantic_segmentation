2729f686484870d3f3dcbba2fdaf666e4e1a75bc
Hermann <blumh@ethz.ch>
2018-05-16 12:44:20 +0200

    add benchmark for out-of-distribution detection

--- experiments/uncertainty_eval.py
+++ experiments/uncertainty_eval.py
@@ -10,11 +10,18 @@ from .evaluation import import_weights_into_network
 from .different_evaluation_parameters import grid_search
 
 
-def evaluate_misclassification_detection(net, data, metric, print_results=True):
-    fpr, tpr, auroc, thresholds = net.misclassification_detection_score(data, metric)
+def evaluate_uncertainty(net, data, metric, benchmark='misclassification',
+                         print_results=True):
+    if benchmark == 'misclassification':
+        fpr, tpr, auroc, thresholds = net.misclassification_detection_score(data, metric)
+    elif benchmark == 'out_of_distribution':
+        fpr, tpr, auroc, thresholds = net.out_of_distribution_detection_score(data,
+                                                                              metric)
+    else:
+        assert False
     if print_results:
-        print('Misclassification Detection of {} on {} with {} metric'.format(
-            net.name, type(data).__name__, metric))
+        print('Uncertainty Benchmark "{}" of {} on {} with {} metric'.format(
+            benchmark, net.name, type(data).__name__, metric))
         print('AUROC {:.3f}'.format(auroc))
         stdout.flush()
     return {'TPR': tpr, 'FPR': fpr, 'AUROC': auroc, 'thresholds': thresholds}
@@ -27,8 +34,9 @@ ex.observers.append(get_mongo_observer())
 
 
 @ex.command
-def misclassification_parameter_search(modelname, net_config, dataset, starting_weights,
-                                       search_parameters, uncertainty_metrics, _run):
+def uncertainty_parameter_search(modelname, net_config, dataset, starting_weights,
+                                 search_parameters, benchmark, uncertainty_metrics,
+                                 _run):
     model = get_model(modelname)
     data = get_dataset(dataset['name'])
 
@@ -36,22 +44,22 @@ def misclassification_parameter_search(modelname, net_config, dataset, starting_
         with model(data_description=data.get_data_description(), **parameters) as net:
             measure_set = data(**dataset).get_measureset()
             import_weights_into_network(net, starting_weights)
-            return {metric: evaluate_misclassification_detection(net, measure_set,
-                                                                 metric,
-                                                                 print_results=False)
+            return {metric: evaluate_uncertainty(net, measure_set, metric,
+                                                 benchmark=benchmark,
+                                                 print_results=False)
                     for metric in uncertainty_metrics}
     _run.info['results'] = grid_search(evaluation, search_parameters, net_config)
 
 
 @ex.automain
-def misclassification_detection(modelname, net_config, dataset, starting_weights,
-                                uncertainty_metrics, _run):
+def uncertainty_benchmark(modelname, net_config, dataset, starting_weights, benchmark,
+                          uncertainty_metrics, _run):
     model = get_model(modelname)
     data = get_dataset(dataset['name'])
     with model(data_description=data.get_data_description(), **net_config) as net:
         data = data(**dataset)
         import_weights_into_network(net, starting_weights)
         for metric in uncertainty_metrics:
-            measurements = evaluate_misclassification_detection(
-                net, data.get_testset(), metric)
+            measurements = evaluate_uncertainty(net, data.get_testset(), metric,
+                                                benchmark=benchmark)
             _run.info.setdefault('measurements', {})[metric] = measurements
--- xview/models/uncertainty_model.py
+++ xview/models/uncertainty_model.py
@@ -21,10 +21,12 @@ class UncertaintyModel(BaseModel):
                     gt, prediction, uncertainty = self.sess.run(
                         [self.evaluation_labels, self.prediction, uncertainty_attr],
                         feed_dict={self.testing_handle: data})
+                    gt = gt.flatten()
+                    prediction = prediction.flatten()
+                    uncertainty = uncertainty.flatten()
                     uncertainty_for_correct_classes.append(uncertainty[gt == prediction])
                     uncertainty_for_misclassification.append(
                         uncertainty[gt != prediction])
-                    break
                 except tf.errors.OutOfRangeError:
                     break
             # reshape uncertainties into 1 big vector
@@ -49,6 +51,55 @@ class UncertaintyModel(BaseModel):
                 fp = np.sum(uncertainty_for_correct_classes > thresholds[i])
                 tpr[i] = tp / n_misclassified
                 fpr[i] = fp / n_correctly_classified
+
+            # area under curve
+            auroc = auc(fpr, tpr, reorder=True)
+
+            return fpr, tpr, auroc, thresholds
+
+    @transform_inputdata()
+    def out_of_distribution_detection_score(self, data, uncertainty_attr,
+                                            num_thresholds=500):
+        with self.graph.as_default():
+            uncertainty_attr = getattr(self, uncertainty_attr)
+
+            uncertainty_in_distribution = []
+            uncertainty_out_of_distribution = []
+
+            while True:
+                try:
+                    # get classification and uncertainty for given dataset
+                    gt, uncertainty = self.sess.run(
+                        [self.evaluation_labels, uncertainty_attr],
+                        feed_dict={self.testing_handle: data})
+                    gt = gt.flatten()
+                    uncertainty = uncertainty.flatten()
+                    uncertainty_in_distribution.append(uncertainty[gt == 0])
+                    uncertainty_out_of_distribution.append(uncertainty[gt == 1])
+                except tf.errors.OutOfRangeError:
+                    break
+            # reshape uncertainties into 1 big vector
+            uncertainty_in_distribution = np.concatenate(
+                uncertainty_in_distribution).flatten()
+            uncertainty_out_of_distribution = np.concatenate(
+                uncertainty_out_of_distribution).flatten()
+
+            # now calculate ROC
+            min_uncertainty = np.min((uncertainty_in_distribution.min(),
+                                      uncertainty_out_of_distribution.min()))
+            max_uncertainty = np.max((uncertainty_in_distribution.max(),
+                                      uncertainty_out_of_distribution.max()))
+            thresholds = np.linspace(min_uncertainty, max_uncertainty,
+                                     num=num_thresholds)
+            tpr = np.zeros(num_thresholds)
+            fpr = np.zeros(num_thresholds)
+            n_in_distribution = uncertainty_in_distribution.shape[0]
+            n_out_of_distribution = uncertainty_out_of_distribution.shape[0]
+            for i in range(num_thresholds):
+                tp = np.sum(uncertainty_out_of_distribution > thresholds[i])
+                fp = np.sum(uncertainty_in_distribution > thresholds[i])
+                tpr[i] = tp / n_out_of_distribution
+                fpr[i] = fp / n_in_distribution
 
             # area under curve
             auroc = auc(fpr, tpr, reorder=True)
