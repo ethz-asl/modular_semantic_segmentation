7adb763b327494cd39dceb90fc01d46776571be5
Hermann <blumh@ethz.ch>
2018-05-23 16:40:56 +0200

    train and evaluate models with label-flipping augmentation

--- experiments/uncertainty_eval.py
+++ experiments/uncertainty_eval.py
@@ -4,10 +4,12 @@ from sacred.utils import apply_backspaces_and_linefeeds
 from xview.models import get_model
 from xview.datasets import get_dataset
 from sys import stdout
+import numpy as np
 
 from .utils import get_mongo_observer
 from .evaluation import import_weights_into_network
 from .different_evaluation_parameters import grid_search
+from .training import create_directories, train_network
 
 
 def evaluate_uncertainty(net, data, metric, benchmark='misclassification',
@@ -25,6 +27,24 @@ def evaluate_uncertainty(net, data, metric, benchmark='misclassification',
         print('AUROC {:.3f}'.format(auroc))
         stdout.flush()
     return {'TPR': tpr, 'FPR': fpr, 'AUROC': auroc, 'thresholds': thresholds}
+
+
+def measure_metrics(net, data, metrics, _run, label_flip=None):
+    nll, class_count = net.nll_score(data)
+    _run.info['measurements'] = {'nll': nll, 'class_counts': class_count}
+    for metric in metrics:
+        _run.info.setdefault('measurements', {})[metric] = net.value_distribution(data,
+                                                                                  metric)
+    _run.info['measurements']['dirichlet_priors'] = net.prob_distribution(data)
+    if label_flip:
+        # measure the difference to the distribution we would expect based on the
+        # label_flip augmentation during the training
+        prior = np.zeros(net.config['num_classes'])
+        prior[label_flip[0]] = 1 - label_flip[2]
+        prior[label_flip[1]] = label_flip[2]
+        _run.info['measurements']['distribution_miscalibration'] = net.mean_diff(
+            data, prior, condition=lambda t, c: np.logical_or(c == label_flip[0],
+                                                              c == label_flip[1]))
 
 
 ex = Experiment()
@@ -49,6 +69,55 @@ def uncertainty_parameter_search(modelname, net_config, dataset, starting_weight
                                                  print_results=False)
                     for metric in uncertainty_metrics}
     _run.info['results'] = grid_search(evaluation, search_parameters, net_config)
+
+
+@ex.command
+def train_ambiguous(modelname, net_config, dataset, starting_weights, method,
+                    num_iterations, uncertainty_metrics, _run):
+    # Set up the directories for diagnostics
+    output_dir = create_directories(_run._id, ex)
+
+    data = get_dataset(dataset['name'])
+    data_description = list(data.get_data_description())
+    num_classes = data_description[2]
+    # augmentate the class labels
+    if method == 'flip_classes':
+        # randomly map two classes onto each other to make them ambiguos
+        classes = np.random.choice(list(range(num_classes)), size=2, replace=False)
+        dataset.setdefault('augmentation', {})['label_flip'] = (classes[0], classes[1],
+                                                                np.random.rand())
+    elif method == 'new_class':
+        # randomly label a given class as a new, nonexisting class
+        data_description[2] = num_classes + 1
+        old_class = np.random.choice(list(range(num_classes)))
+        dataset.setdefault('augmentation', {})['label_flip'] = (old_class, num_classes,
+                                                                np.random.rand())
+    elif method == 'merge':
+        # randomly merge two classes together
+        classes = np.random.choice(list(range(num_classes)), size=2, replace=False)
+        dataset.setdefault('augmentation', {})['label_merge'] = (classes[0], classes[1])
+    _run.info.setdefault('dataset', {}).update(dataset)
+
+    model = get_model(modelname)
+    with model(data_description=data_description, output_dir=output_dir,
+               **net_config) as net:
+        data = data(**dataset)
+        train_network(net, output_dir, data, num_iterations, starting_weights, ex)
+        measure_metrics(net, data.get_testset(), uncertainty_metrics, _run,
+                        label_flip=dataset['augmentation'].get('label_flip', None))
+
+
+@ex.command
+def measure(modelname, net_config, dataset, starting_weights, uncertainty_metrics, _run):
+    model = get_model(modelname)
+    data = get_dataset(dataset['name'])
+    data_description = list(data.get_data_description())
+    if 'num_classes' in dataset:
+        data_description[2] = dataset['num_classes']
+    with model(data_description=data_description, **net_config) as net:
+        data = data(**dataset)
+        import_weights_into_network(net, starting_weights)
+        measure_metrics(net, data.get_testset(), uncertainty_metrics, _run)
 
 
 @ex.automain
